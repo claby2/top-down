a640d0e864afe16bb82d5ed4881a0f12b16b710c
claby2 <wibow9770@gmail.com>
2020-05-16 16:16:59 +0800

    disabled visibility of default cursor and replaced with crosshair

---
+++ Assets/CrosshairBehavior.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrosshairBehavior : MonoBehaviour {
+    public Camera MainCamera;
+
+    void Start() {
+        Cursor.visible = false;
+    }
+
+    void FixedUpdate() {
+        Vector2 cursorPos = MainCamera.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = cursorPos;
+    }
+}
